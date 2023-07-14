0f664aaba0a51e9850d2f099890fc20e0ff14644
JoshHaase0 <joshua.m.haase@gmail.com>
2023-07-14 09:07:08 -0500

    final commit

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var projectRouter = require('./routes/projects');
+var projectRouter = require('./routes/projects').router;
 
 var app = express();
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
+var projects = require("./projects").getProjects();
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   res.send('<p>HTML Data</p>');
 });
+
+router.get("/projects", (req, res) => {
+  res.send(projects.sort((a, b) => a.id - b.id));
+});
+router.get("/projects/active", (req, res) => {
+  res.send(projects.filter((_) => _.isActive))
+});
+router.get("/projects/:id", (req, res) => {
+  const id = req.params.id;
+  const idProj = projects.filter((_) => _.id.toString() === id)
+  const exists = idProj.length > 0 ? true : false;
+  if (exists) {
+    res.send(idProj[0]);
+  } else {
+    res.status(404);
+    res.send({message: "No Project Found"});
+  }
+});
+
 
 module.exports = router;
